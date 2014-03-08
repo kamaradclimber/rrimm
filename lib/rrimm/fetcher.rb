8af8401cb70c618546318b474d01f15e130e9dfc
Gregoire Seux <kamaradclimber@gmail.com>
2014-03-08 13:18:33 +0100

    Fetch feeds in parallel

--- lib/rrimm/fetcher.rb
+++ lib/rrimm/fetcher.rb
@@ -1,6 +1,7 @@
 require 'rss'
 require 'open-uri'
 require 'open_uri_redirections'
+require 'parallel'
 
 module RSS
   module Atom
@@ -23,20 +24,30 @@ module RRImm
       @config = config
     end
 
+    def parallel_fetch(concurrency)
+      Parallel.map(@config.feeds, :in_threads => concurrency) do |name,feed_config|
+        fetch_feed(name, feed_config)
+      end
+    end
+
     def fetch
       @config.feeds.map do |name,feed_config|
-        last_read = Time.at(@config.get_cache.read(feed_config))
-        open(feed_config.uri, :allow_redirections => :safe) do |rss|
-          feed = RSS::Parser.parse(rss)
-          feed = GenericFeed.new feed
-          items = feed.items.select { |item| item.date > last_read }
-          last_read = items.collect { |item| item.date }.max unless items.empty?
-          items.each do |item|
-            feed_config.format(feed, item)
-          end
-        end
-        @config.get_cache.save(feed_config, last_read.to_i)
+        fetch_feed(name, feed_config)
       end
+    end
+
+    def fetch_feed(name, feed_config)
+      last_read = Time.at(@config.get_cache.read(feed_config))
+      open(feed_config.uri, :allow_redirections => :safe) do |rss|
+        feed = RSS::Parser.parse(rss)
+        feed = GenericFeed.new feed
+        items = feed.items.select { |item| item.date > last_read }
+        last_read = items.collect { |item| item.date }.max unless items.empty?
+        items.each do |item|
+          feed_config.format(feed, item)
+        end
+      end
+      @config.get_cache.save(feed_config, last_read.to_i)
     end
   end
 end
