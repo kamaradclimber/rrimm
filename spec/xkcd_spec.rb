7a9ad300820e7bc19c0e1c55d0530bbe28570b52
Gregoire Seux <kamaradclimber@gmail.com>
2014-03-09 19:04:11 +0100

    Add test for default formatter

--- spec/xkcd_spec.rb
+++ spec/xkcd_spec.rb
@@ -19,3 +19,16 @@ describe RRImm::ItemFormatter::Mail do
     expect(s.string).to eq "From: RRImm <from@example.com>\nTo: to@example.com\nSubject: When You Assume\nContent-Type: text/html;\n\nhttp://xkcd.com/1339/\n\n<img src=\"http://imgs.xkcd.com/comics/when_you_assume.png\" title=\"You know what happens when you assert--you make an ass out of the emergency response team.\" alt=\"You know what happens when you assert--you make an ass out of the emergency response team.\" />\n"
   end
 end
+
+describe RRImm::ItemFormatter::Default do
+  it 'formats correctly xkcd feed' do
+    default_formatter = RRImm::ItemFormatter::Default.new
+    xkcd_file = File.join('file://', File.dirname(__FILE__), 'xkcd.xml')
+    feed = Feedzirra::Feed.fetch_and_parse(xkcd_file)
+    expect(feed.entries.size).to be > 0
+    s = StringIO.new
+    default_formatter.format(feed, feed.entries.first, nil, s)
+    s.close
+    expect(s.string).to eq "xkcd.com: When You Assume (2014-03-07 05:00:00 UTC)\n"
+  end
+end
