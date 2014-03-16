89ea8941908cfa2a61cb75d982fe82fe25ee6309
Gregoire Seux <kamaradclimber@gmail.com>
2014-03-16 20:39:44 +0100

    Also test with concurrency

--- spec/fetcher_spec.rb
+++ spec/fetcher_spec.rb
@@ -1,28 +1,37 @@
 require_relative 'spec_helper'
 
-
-
 describe RRImm::Fetcher do
+  let(:basic_conf) do
+    xkcd_file = File.join('file://', File.dirname(__FILE__), 'xkcd.xml')
+    feed = RRImm::Feed.new xkcd_file do
+      pipe 'cat > /dev/null'
+    end
+    cache = double('cache_mock')
+    allow(RRImm::Cache).to receive(:new).and_return(cache)
+    allow(cache).to receive(:path)
+
+    allow(cache).to receive(:read).and_return(3829)
+    allow(cache).to receive(:save)
+
+    config = RRImm::Config.new
+    config.feeds['local_xkcd'] = feed
+    config
+  end
+
   describe '#initialize' do
+    it 'createscorrectly' do
+      expect { RRImm::Fetcher.new basic_conf }.not_to raise_error
+    end
+  end
+
+  describe '#fetch' do
     it 'fetches correctly' do
-      xkcd_file = File.join('file://', File.dirname(__FILE__), 'xkcd.xml')
-      feed = RRImm::Feed.new xkcd_file do
-        pipe 'cat > /dev/null'
-      end
-      cache = double('cache_mock')
-      allow(RRImm::Cache).to receive(:new).and_return(cache)
-      allow(cache).to receive(:path)
-
-      allow(cache).to receive(:read).and_return(3829)
-      allow(cache).to receive(:save)
-
-      config = RRImm::Config.new
-      config.feeds['local_xkcd'] = feed
-      
-      fetcher = RRImm::Fetcher.new config
-
-
+      fetcher = RRImm::Fetcher.new basic_conf
       expect { fetcher.fetch }.not_to raise_error
+    end
+    it 'fetches correctly when using concurrency' do
+      fetcher = RRImm::Fetcher.new basic_conf
+      expect { fetcher.fetch(5) }.not_to raise_error
     end
   end
 end
